019533ecff17e1aa93035fea5f55106b02bad792
Bharti <bhartikumari112000@gmail.com>
2024-07-02 14:42:44 +0530

    module 1 added

---
+++ python/module1.py
@@ -0,0 +1,14 @@
+# What is Software?
+## Software is a set of instructions, programs or data which is used for computers or devices to perform a specific task.
+
+# What are the types of Applications?
+## Some of the types of application software includes - spreadsheet software, word processing software, presentation software,
+# open source softwware, closed source software, educational software, graphics software, multimedia software, web browser,
+# shareware, simulation software etc.
+
+# What is programing?
+## Programming is the process of creating set of instructions for the computer to perform a specific task.
+
+# What is Python?
+## Python is a high level, interpreter based, open source, object oriented programming language used in web application,
+# software development, data science, machine learning and related research as well as deployment of models.
