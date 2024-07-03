a6261ee87f94ae3008ac5db48b88eddf17c797ad
Bharti <bhartikumari112000@gmail.com>
2024-07-03 22:59:32 +0530

    len of the string done

---
+++ python/q12-len_of_str.py
@@ -0,0 +1,13 @@
+# Write a Python program to calculate the length of a string.
+
+#method-1
+str = input("enter the string: ")
+print(len(str))
+
+#method-2
+str1 = input("enter the string: ")
+counter = 0
+for i in str1:
+    counter = counter+1
+    length = counter
+print(length)
