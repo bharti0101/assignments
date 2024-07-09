a01db67dccf0760340c58bf51653c4c66ff11098
Bharti <bhartikumari112000@gmail.com>
2024-07-09 21:08:13 +0530

    q-27 add, min, max list ele done

--- python/q-27 large,small,sum_list.py
+++ python/q-27 large,small,sum_list.py
@@ -33,3 +33,46 @@ minimum = smallest(li)
 print(minimum)
 addition = sumofelements(li)
 print(addition)
+
+#method - 2
+
+lis = [2, 5, 0, 6, 1, 4, 3]
+def findmin(listele):
+    return min(listele)
+def findmax(listele):
+    return max(listele)
+def findsum(listele):
+    return sum(listele)
+print(findmin(lis))
+print(findmax(lis))
+print(findsum(lis))
+
+#method - 3
+
+n = int(input("enter the no of elements to be inserted: "))
+lis = []
+print("enter the elements: ")
+for i in range(0,n):
+    ele = int(input())
+    lis.append(ele)
+lis.sort()
+print(lis)
+def findminimum(sortedlist):
+    return sortedlist[0]
+def findmaximum(sortedlist):
+    return sortedlist[-1]
+def findsum(sortedlist):
+    return sum(sortedlist)
+print(findminimum(lis))
+print(findmaximum(lis))
+print(findsum(lis))
+
+#method - 4
+
+li = input("enter the elements: ").split()
+li2=[]
+for i in li:
+    li2.append(int(i))
+
+li2.sort()
+print(f"the min value is: {li2[0]}, the max value is: {li2[-1]}, the sum of the elements is: {sum(li2)}")
