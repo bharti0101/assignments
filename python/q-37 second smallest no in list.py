f986cfd265c37cd73117ab7d396fbb4602a7d860
Bharti <bhartikumari112000@gmail.com>
2024-09-28 18:39:58 +0530

    q-37

---
+++ python/q-37 second smallest no in list.py
@@ -0,0 +1,23 @@
+#Write a Python program to find the second smallest number in a list.
+li = [23,4,94,11,30,48]
+sorted_li = sorted(li)
+print(f"second smallest no: {sorted_li[1]}")
+
+
+
+##
+# li = [23,4,94,11,30,48]
+# smallest = li[0]
+# smallest2=li[0]
+# for i in li:
+#     if smallest > i:
+#        smallest = i
+#
+#     else:
+#         continue
+
+
+
+
+
+
